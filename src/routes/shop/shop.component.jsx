461e86788029e79b2db6007d24865c3f1cdd237a
samirfarhi <samirfrh1990@gmail.com>
2023-03-15 08:37:54 +0100

    adding redux-thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from 'react-redux'
 
 import './shop.styles.scss'
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase'
-import { setCategories } from '../../store/categories/category.action'
+import { fetchCategoriesAsync } from '../../store/categories/category.action'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
@@ -13,12 +12,7 @@ import Category from '../category/category.component'
 const Shop = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments()
-      dispatch(setCategories(categoriesArray))
-    }
-
-    getCategoriesMap()
+    dispatch(fetchCategoriesAsync())
   })
   return (
     <Routes>
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,25 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types'
 import { createAction } from '../../utils/reducer/reducer.utils'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase'
 
 export const setCategories = (categoriesArray) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray)
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+
+export const fetchCategoriesSuccess = (categories) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories)
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart())
+  try {
+    const categoriesArray = await getCategoriesAndDocuments()
+    dispatch(fetchCategoriesSuccess(categoriesArray))
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error))
+  }
+}
